8168a0c2a23bfe049f9af6a01b1438dc8cd848d8
JinShil <slavo5150@yahoo.com>
2022-11-24 15:49:17 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Gtk;
+using BlazorWebkit;
+        
+Application.Init();
+
+var window = new Window(WindowType.Toplevel);
+window.DefaultSize = new Gdk.Size(1024, 768);
+// window.Fullscreen();
+
+window.DeleteEvent += (o, e) =>
+{
+    Application.Quit();
+};
+
+var webView = new BlazorWebView();
+window.Add(webView);
+
+window.ShowAll();
+
+Application.Run();
+
