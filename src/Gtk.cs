1aea4dd8cc164ea1c2402093c172e718fc935ffe
JinShil <slavo5150@yahoo.com>
2022-11-24 15:52:59 +0900

    Restructor directories

---
+++ src/Gtk.cs
@@ -0,0 +1,34 @@
+using System.Runtime.InteropServices;
+
+namespace BlazorWebkit;
+
+internal static class Gtk
+{
+    public const string FilePath = "gtk";
+
+    public enum GtkWindowType : int
+    {
+        GTK_WINDOW_TOPLEVEL,
+        GTK_WINDOW_WINDOW_POPUP
+    }
+
+    public enum GConnectFlags : int
+    {
+        G_CONNECT_AFTER,  	 
+        G_CONNECT_SWAPPED
+    }
+
+    [DllImport(FilePath)]
+    public static extern ulong g_signal_connect_data (nint instance, string detailed_signal, nint c_handler, nint data, nint destroy_data, GConnectFlags connect_flags);
+
+    public static ulong g_signal_connect(nint instance, string detailed_signal, nint c_handler, nint data)
+    {
+        return g_signal_connect_data(instance, detailed_signal, c_handler, data, nint.Zero,  (GConnectFlags)0);
+    }
+
+    [DllImport(FilePath)]
+    public static extern nint g_memory_input_stream_new_from_data (byte[] data, uint len, nint destroy);
+
+    [DllImport(FilePath)]
+    public static extern void g_free (IntPtr o);
+}
---
+++ src/WebKit.cs
@@ -0,0 +1,48 @@
+using System.Runtime.InteropServices;
+
+namespace BlazorWebkit;
+
+internal static class WebKit
+{
+    public enum WebKitUserContentInjectedFrames
+    {
+        WEBKIT_USER_CONTENT_INJECT_ALL_FRAMES = 0,
+        WEBKIT_USER_CONTENT_INJECT_TOP_FRAME = 1
+    }
+
+    public enum WebKitUserScriptInjectionTime
+    {
+        WEBKIT_USER_SCRIPT_INJECT_AT_DOCUMENT_START = 0,
+        WEBKIT_USER_SCRIPT_INJECT_AT_DOCUMENT_END = 1
+    }
+
+    public const string FilePath = "webkit";
+
+    [DllImport(FilePath)]
+    public static extern nint webkit_user_script_new(string source, WebKitUserContentInjectedFrames injected_frames,
+        WebKitUserScriptInjectionTime injection_time, string? allow_list, string? block_list);
+
+    [DllImport(FilePath)]
+    public static extern void webkit_user_content_manager_add_script(nint manager, nint script);
+
+    [DllImport(FilePath)]
+    public static extern void webkit_user_script_unref(nint script);
+
+    [DllImport(FilePath)]
+    public static extern bool webkit_user_content_manager_register_script_message_handler(nint manager, string name);
+
+    [DllImport(FilePath)]
+    public static extern void webkit_web_view_run_javascript (nint web_view, string script, nint cancellable, nint callback, nint user_data);
+
+    [DllImport(FilePath)]
+    public static extern void webkit_javascript_result_unref(nint js_result);
+
+    [DllImport(FilePath)]
+    public static extern nint webkit_javascript_result_get_js_value(nint js_result);
+
+    [DllImport(FilePath)]
+    public static extern bool jsc_value_is_string(nint value);
+
+    [DllImport(FilePath)]
+    public static extern nint jsc_value_to_string(nint value);
+}
