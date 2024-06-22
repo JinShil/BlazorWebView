81f0ab26f5cf6c6b8f4537786f5a150cd59e12b3
JinShil <slavo5150@yahoo.com>
2024-06-22 14:18:05 +0900

    Fix issue where Ctrl-C would not exit the demo application.

--- WebKitGtk.Test/Program.cs
+++ WebKitGtk.Test/Program.cs
@@ -5,17 +5,19 @@ using Microsoft.Extensions.Hosting;
 using System.Runtime.Versioning;
 using WebKitGtk.Test.Data;
 using System.Threading.Tasks;
-using System.Security.Cryptography.X509Certificates;
+
+using System.Threading;
+using System;
 
 [UnsupportedOSPlatform("OSX")]
 [UnsupportedOSPlatform("Windows")]
-internal class Program
+internal class Program : IHostedService
 {
-	private static int Main(string[] args)
+	private static async Task Main(string[] args)
 	{
-		var appBuilder = Host.CreateApplicationBuilder(args);
+		var builder = Host.CreateApplicationBuilder(args);
 		//appBuilder.Logging.AddDebug();
-		appBuilder.Logging.AddSimpleConsole(
+		builder.Logging.AddSimpleConsole(
 			options => {
 				options.ColorBehavior = Microsoft.Extensions.Logging.Console.LoggerColorBehavior.Disabled;
 				options.IncludeScopes = false;
@@ -24,43 +26,71 @@ internal class Program
 		})
 		.SetMinimumLevel(LogLevel.Information);
 
-		appBuilder.Services.AddBlazorWebViewOptions(
+		builder.Services.AddBlazorWebViewOptions(
 			new BlazorWebViewOptions()
 			{
 				RootComponent = typeof(WebKitGtk.Test.App),
 				HostPath = "wwwroot/index.html"
 			}
 		)
-		.AddSingleton<WeatherForecastService>();
+		.AddSingleton<WeatherForecastService>()
+		.AddHostedService<Program>();
 
-		using var myApp = appBuilder.Build();
+		using var host = builder.Build();
 
-		myApp.Start();
-
-		try
-		{
-			WebKit.Module.Initialize();
-
-			var application = Adw.Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
-
-			application.OnActivate += (sender, args) =>
-			{
-				var window = Gtk.ApplicationWindow.New((Adw.Application)sender);
-				window.Title = "Blazor";
-				window.SetDefaultSize(800, 600);
-
-				var webView = new BlazorWebView(myApp.Services);
-				window.SetChild(webView);
-				window.Show();
-				// Allow opening developer tools
-				webView.GetSettings().EnableDeveloperExtras = true;
-			};
-
-			return application.Run(0, []);
-		}
-		finally
-		{
-			Task.Run(async () => await myApp.StopAsync()).GetAwaiter().GetResult();
-		}
+		await host.RunAsync();
 	}
+
+	public Program(IHostApplicationLifetime lifetime, IServiceProvider serviceProvider)
+	{
+		WebKit.Module.Initialize();
+
+		_serviceProvider = serviceProvider;
+		_app = Adw.Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
+
+		_app.OnActivate += (sender, args) =>
+		{
+			var window = Gtk.ApplicationWindow.New((Adw.Application)sender);
+			window.Title = "Blazor";
+			window.SetDefaultSize(800, 600);
+
+			var webView = new BlazorWebView(_serviceProvider);
+			window.SetChild(webView);
+			window.Show();
+
+			// Allow opening developer tools
+			webView.GetSettings().EnableDeveloperExtras = true;
+		};
+
+		_app.OnShutdown += (sender, args) =>
+		{
+			lifetime.StopApplication();
+		};
+
+		lifetime.ApplicationStarted.Register(() => 
+		{
+			Task.Run(() => 
+			{
+				Environment.ExitCode = _app.Run(0, []);
+			});
+		});
+
+		lifetime.ApplicationStopping.Register(() =>
+		{
+			_app.Quit();
+		});
+	}
+
+	readonly IServiceProvider _serviceProvider;
+	readonly Adw.Application _app;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
 }
